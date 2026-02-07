8e67158aa35278485aa43e92b66eb10a39b0f31f
daniisnotavailable <mamanibeltran20daniel@gmail.com>
2026-02-07 16:05:09 +0000

    feat: creación de nivel base, paredes con colisión

--- pawn-expedition/Scenes/Player/jugador.gd
+++ pawn-expedition/Scenes/Player/jugador.gd
@@ -1,3 +1,11 @@
 extends CharacterBody2D
 
 class_name Jugador
+@export var stats: DataPiezas
+
+@onready var pivot: Node2D = $Pivot
+@onready var animated_sprite_2d: AnimatedSprite2D = %AnimatedSprite2D
+
+var can_move := true
+var movement: Vector2
+var direction: Vector2
