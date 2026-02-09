d0f3a4b5e5e2779dc33ee9cfa8077c3adf207699
daniisnotavailable <mamanibeltran20daniel@gmail.com>
2026-02-09 04:11:17 +0000

    feat: logica personaje

---
+++ pawn-expedition/Scenes/Niveles/sala_nivel_01.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+@export var obstaculo_scene: PackedScene
+@export var player_scene: PackedScene
+
+@export var spawn_area_min: Vector2 = Vector2(64, 64)
+@export var spawn_area_max: Vector2 = Vector2(320, 320)
+@export var spawn_player_pos: Vector2 = Vector2(190, 190)
+
+func _ready() -> void:
+	spawn_obstaculos()
+	spawn_players()
+	
+func spawn_obstaculos() -> void:
+	var count: int = GameGlobal.get_box_count_for_level()
+	for i in range(count):
+		var obstaculo: StaticBody2D = obstaculo_scene.instantiate()
+		var pos_x = randf_range(spawn_area_min.x, spawn_area_max.x)
+		var pos_y = randf_range(spawn_area_min.y, spawn_area_max.y)
+		obstaculo.position = Vector2(pos_x, pos_y)
+		add_child(obstaculo)
+		
+func spawn_players() -> void:
+	if player_scene:
+		var jugador: CharacterBody2D = player_scene.instantiate()
+		jugador.position = spawn_player_pos
+		add_child(jugador)
+	
+func _on_puerta_body_entered(body: Node2D) -> void:
+	if body.name == "Jugador" or body is CharacterBody2D:
+		print("Pasando de nivel")
+		if GameGlobal.advance_level():
+			get_tree().reload_current_scene()
+		else:
+			print("Terminado")
+			GameGlobal.reset_run()
+			get_tree().reload_current_scene()
