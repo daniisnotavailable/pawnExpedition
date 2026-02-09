1c7469b3e8b2d8f54b20f902756d9c956121f666
daniisnotavailable <mamanibeltran20daniel@gmail.com>
2026-02-09 05:53:51 +0000

    update: arreglado el movimiento

--- pawn-expedition/Scenes/Niveles/sala_nivel_01.gd
+++ pawn-expedition/Scenes/Niveles/sala_nivel_01.gd
@@ -3,15 +3,17 @@ extends Node2D
 @export var obstaculo_scene: PackedScene
 @export var player_scene: PackedScene
 
-@export var spawn_area_min: Vector2 = Vector2(64, 64)
-@export var spawn_area_max: Vector2 = Vector2(320, 320)
-@export var spawn_player_pos: Vector2 = Vector2(190, 190)
+@export var spawn_area_min: Vector2 = Vector2(-200, -200)
+@export var spawn_area_max: Vector2 = Vector2(200, 200)
+@export var spawn_player_pos: Vector2 = Vector2(0, 0)
 
 func _ready() -> void:
 	spawn_obstaculos()
 	spawn_players()
 	
 func spawn_obstaculos() -> void:
+	if not obstaculo_scene: return
+	
 	var count: int = GameGlobal.get_box_count_for_level()
 	for i in range(count):
 		var obstaculo: StaticBody2D = obstaculo_scene.instantiate()
--- pawn-expedition/Scenes/Player/jugador.gd
+++ pawn-expedition/Scenes/Player/jugador.gd
@@ -2,8 +2,8 @@ extends CharacterBody2D
 
 class_name Jugador
 @export var move_speed := 200
-@onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
-@onready var sprite: AnimatedSprite2D = %AnimatedSprite2D
+@onready var sprite: AnimatedSprite2D = $Pivot/AnimatedSprite2D
+var target_position: Vector2
 
 var hp: float
 var gold: int
@@ -11,32 +11,28 @@ var gold: int
 func _ready() -> void:
 	hp = GameGlobal.player_hp
 	gold = GameGlobal.player_gold
-	
-	nav_agent.path_desired_distance = 4.0
-	nav_agent.target_desired_distance = 4.0
-	
+	target_position = global_position
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			nav_agent.target_position = get_global_mouse_position()
-			
-func _physics_process(delta: float) -> void:
-	if nav_agent.is_navigation_finished():
+			target_position = get_global_mouse_position()
+
+func _physics_process(_delta: float) -> void:
+	if global_position.distance_to(target_position) > 10.0:
+		var direction = global_position.direction_to(target_position)
+		velocity = direction * move_speed
+		move_and_slide()
+		if direction.x > 0:
+			sprite.flip_h = false
+		elif direction.x < 0:
+			sprite.flip_h = true
+		if sprite.sprite_frames.has_animation("walk"):
+			sprite.play("walk")
+	else:
+		velocity = Vector2.ZERO
 		if sprite.sprite_frames.has_animation("iddle"):
 			sprite.play("iddle")
-		return
-
-	var next_pos: Vector2 = nav_agent.get_next_path_position()
-	var direction: Vector2 = global_position.direction_to(next_pos)
-	velocity = direction * move_speed
-	move_and_slide()
-	
-	if (direction.x > 0.1):
-		sprite.flip_h = false
-	elif (direction.x < -0.1):
-		sprite.flip_h = true
-	if (sprite.sprite_frames.has_animation("move")):
-		sprite.play("move")
 	
 func take_damage (amount: float) -> void:
 	hp -= amount
